3131e05f5e9838211a768fd3e78bd486c8c8cb34
jadynesmith <jadynesmith.dev@gmail.com>
2026-04-12 17:28:34 -0700

    Update AssignMe page index

---
+++ subdomains/assign-me/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            AssignMe
+        </title>
+    </head>
+    <body>
+        <p>
+            Welcome to my AssignMe project page!
+
+            <br><br>
+
+            This project is a web application that allows college students to manage & track their assignments.
+        </p>
+    </body>
+</html>
